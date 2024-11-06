4fa40981a6ecac497e3eb36eea54a7a8447decaf
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-11-05 21:12:13 -0600

    feat: Add command line arguments
    The -h and -v command line arguments are now supported, which displays a
    usage statement and version information.

--- src/main.c
+++ src/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
+#include "args.h"
 #include "haversine.h"
+#include "version.h"
 
-int main() {
-  Coord london = {51.5074, -0.1278};
-  Coord caracas = {10.4806, -66.9036};
+int main(int argc, char *argv[]) {
+  add_arg('h', "help", "Print this usage message.", ARG_NONE);
+  add_arg('v', "version", "Print version information.", ARG_NONE);
 
-  double d = haversine(london, caracas);
+  if (get_arg_bool(argc, argv, 'h', false)) {
+    usage(argv[0]);
+  }
+
+  if (get_arg_bool(argc, argv, 'v', false)) {
+    version(VERSION_STRING, LICENSE_STRING);
+    exit(EXIT_SUCCESS);
+  }
+
+  Coord c1 = {51.5074, -0.1278};
+  Coord c2 = {10.4806, -66.9036};
+
+  double d = haversine(c1, c2);
   printf("Distance: %f km\n", d);
 }
