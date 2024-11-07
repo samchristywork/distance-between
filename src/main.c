a56a3fdc5fe2fb4b7987dcecbd67cce3c175aec9
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-11-06 19:15:40 -0600

    feat: Implement user-supplied latitude and longitude coordinates
    The user can now supply coordinates using the `--lat1`, --`lon1`, etc...
    command line arguments.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "args.h"
 #include "haversine.h"
 #include "version.h"
 
 int main(int argc, char *argv[]) {
+  add_arg('a', "lat1", "The latitude part of the first coordinate.",
+          ARG_REQUIRED);
+  add_arg('b', "lon1", "The longitude part of the first coordinate.",
+          ARG_REQUIRED);
+  add_arg('c', "lat2", "The latitude part of the second coordinate.",
+          ARG_REQUIRED);
+  add_arg('d', "lon2", "The longitude part of the second coordinate.",
+          ARG_REQUIRED);
   add_arg('h', "help", "Print this usage message.", ARG_NONE);
   add_arg('v', "version", "Print version information.", ARG_NONE);
 
@@ -17,8 +26,23 @@ int main(int argc, char *argv[]) {
     exit(EXIT_SUCCESS);
   }
 
-  Coord c1 = {51.5074, -0.1278};
-  Coord c2 = {10.4806, -66.9036};
+  bool is_set[] = {false, false, false, false};
+  double lat1 = get_arg_double(argc, argv, 'a', 0, &is_set[0]);
+  double lon1 = get_arg_double(argc, argv, 'b', 0, &is_set[1]);
+  double lat2 = get_arg_double(argc, argv, 'c', 0, &is_set[2]);
+  double lon2 = get_arg_double(argc, argv, 'd', 0, &is_set[3]);
+
+  for (int i = 0; i < 4; i++) {
+    if (!is_set[i]) {
+      usage(argv[0]);
+    }
+  }
+
+  // Coord london = {51.5074, -0.1278};
+  // Coord caracas = {10.4806, -66.9036};
+
+  Coord c1 = {lat1, lon1};
+  Coord c2 = {lat2, lon2};
 
   double d = haversine(c1, c2);
   printf("Distance: %f km\n", d);
